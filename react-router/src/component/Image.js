9e298610bddf36f8a4bfabb106d02ce5b1e4419b
Daniel Charles <danielcharles_500@hotmail.com>
2024-07-02 09:32:48 +0100

    added Loading state

--- react-router/src/component/Image.js
+++ react-router/src/component/Image.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 const Image = () => {
     const [imageURL, setImageURL] = useState(null);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
@@ -13,18 +14,18 @@ const Image = () => {
                 return response.json();
             })
             .then((response) => setImageURL(response[0].url))
-            .catch((error) => setError(error.message));
+            .catch((error) => setError(error.message))
+            .finally(() => setLoading(false));
     }, []);
 
+    if (loading) return <p>Loading...</p>;
     if (error) return <p>A network error was encountered: {error}</p>;
 
     return (
-        imageURL && (
-            <>
-                <h1>An image</h1>
-                <img src={imageURL} alt={"placeholder text"} />
-            </>
-        )
+        <>
+            <h1>An image</h1>
+            <img src={imageURL} alt={"placeholder text"} />
+        </>
     );
 };
 
