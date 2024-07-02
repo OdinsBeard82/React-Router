e5b290d00adbb3d1fb315bc07084a20adc1c5501
Daniel Charles <danielcharles_500@hotmail.com>
2024-07-02 09:36:42 +0100

    custom hook created

--- react-router/src/component/Image.js
+++ react-router/src/component/Image.js
@@ -1,22 +1,8 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import useImageURL from "../hooks/useImageURL";
 
 const Image = () => {
-    const [imageURL, setImageURL] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
-            .then((response) => {
-                if (response.status >= 400) {
-                    throw new Error("Server error");
-                }
-                return response.json();
-            })
-            .then((response) => setImageURL(response[0].url))
-            .catch((error) => setError(error.message))
-            .finally(() => setLoading(false));
-    }, []);
+    const { imageURL, error, loading } = useImageURL();
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>A network error was encountered: {error}</p>;
